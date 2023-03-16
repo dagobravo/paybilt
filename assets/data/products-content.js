62b3259108c22e17fcec08c269ab5fb6882f0ad3
Lester D. Bravo <dago@outlook.com>
2023-03-15 21:24:08 -0600

    Latest changes

---
+++ assets/data/products-content.js
@@ -0,0 +1,51 @@
+const productsContent = [
+  {
+    label: "Products",
+    items: [
+      {
+        linkText: "Point-of-Sale (POS)",
+        headerLabel: "Point-of-Sale (POS)",
+        headerText: "Bridging the gap between online and brick-and-mortar payments.",
+        contentText: "Paybilt is the first to bring a suite of alternative payments to in-person transactions. As the line becomes more and more blurred between online and in-store sales, returns, and exchanges, this will be key in continuing its growth as a platform.",
+        features: [
+          { featureText: "Does not require customer registration with Paybilt" },
+          { featureText: "Customers can purchase larger ticket items which usually require a bank draft or certified cheque" },
+          { featureText: "Other Technologies: Pay-by-links, request invoices, and more" }
+        ]
+      },
+      {
+        linkText: "Express Checkout",
+        headerLabel: "Express Checkout",
+        headerText: "Paybilt's express eTransfer® enhances the payment process every step of the way.",
+        contentText:
+          `Canada's favourite way to pay has been fully automated by Paybilt, making it the perfect payment method for high-volume online businesses.
+            Add Paybilt's e-Transfer alongside your current payment options and gain access to more than 24 million active monthly e-Transfer® users. Overnight.
+
+            The proof is in the payments.
+                  82% of Canadians would like to see businesses accept Interac e-Transfer® as a payment method.
+                  68% of Canadians would rather pay with an Interac e-Transfer® than a credit card.
+                  76% of Canadians would prefer to be refunded with an Interac e-Transfer®.
+            The numbers don't lie. Canadians love it.`,
+        features: [
+          { featureText: "NO CUSTOMER REGISTRATION REQUIRED. Customers conduct payments using their online banking login information." },
+          { featureText: "PURCHASE LARGER TICKET ITEMS. Customers can purchase items which usually require a bank draft or certified cheque." },
+          { featuretext: "ONE-CLICK CHECKOUT. Offer your customers the quickest payment experiences possible." },
+          { featureText: "EXPAND YOUR CHECKOUT IN A FLASH. Be up and running in as little as 1 business day." },
+          { featuretext: "OPTIONAL INSTANT PROCESSING. Payments made within seconds. This is the digital age – should your payments not reflect that?" },
+          { featureText: "EASY CUSTOMIZATION. Match your brand by customizing the logo, font, and colors." },
+          { featureText: "FEWER FIELDS. Get shoppers to the finish line faster. With fewer fields you significantly decrease the purchase time and reduce the possibility of user error." },
+          { featureText: "MOBILE READY. Provide the best checkout experience regardless of the device with checkout optimized for mobile." }
+        ]
+      }
+    ]
+  },
+  {
+    label: "International"
+  },
+  {
+    label: "Payments (IN)"
+  },
+  {
+    label: "Payments (OUT)"
+  }
+];

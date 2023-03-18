67db8379b45fad87e6129b86e860bab23615bf8a
Lester D. Bravo <dago@outlook.com>
2023-03-17 21:02:16 -0600

    Latest changes

---
+++ assets/data/products.js
@@ -0,0 +1,214 @@
+const productsContent = [
+  {
+    label: "Products",
+    items: [
+      {
+        linkText: "Point-of-Sale (POS)",
+        headerLabel: "Point-of-Sale (POS)",
+        headerText: "Bridging the gap between online and brick-and-mortar payments.",
+        contentText: "Paybilt is the first to bring a suite of alternative payments to in-person transactions. As the line becomes more and more blurred between online and in-store sales, returns, and exchanges, this will be key in continuing its growth as a platform.",
+        features: [
+          { featureText: "Does not require customer registration with Paybilt" },
+          { featureText: "Customers can purchase larger ticket items which usually require a bank draft or certified cheque" },
+          { featureText: "Other Technologies: Pay-by-links, request invoices, and more" }
+        ]
+      },
+      {
+        linkText: "Express Checkout",
+        headerLabel: "Express Checkout",
+        headerText: "Paybilt's express eTransfer® enhances the payment process every step of the way.",
+        contentText: `
+          Canada's favourite way to pay has been fully automated by Paybilt, making it the perfect payment method for high-volume online businesses.
+          Add Paybilt's e-Transfer alongside your current payment options and gain access to more than 24 million active monthly e-Transfer® users.
+          Overnight. The proof is in the payments.
+            <ul>
+              <li>82% of Canadians would like to see businesses accept Interac e-Transfer® as a payment method.</li>
+              <li>68% of Canadians would rather pay with an Interac e-Transfer® than a credit card.</li>
+              <li>76% of Canadians would prefer to be refunded with an Interac e-Transfer®. The numbers don't lie. Canadians love it.</li>
+            </ul>`,
+        features: [
+          { featureText: "NO CUSTOMER REGISTRATION REQUIRED. Customers conduct payments using their online banking login information." },
+          { featureText: "PURCHASE LARGER TICKET ITEMS. Customers can purchase items which usually require a bank draft or certified cheque." },
+          { featuretext: "ONE-CLICK CHECKOUT. Offer your customers the quickest payment experiences possible." },
+          { featureText: "EXPAND YOUR CHECKOUT IN A FLASH. Be up and running in as little as 1 business day." },
+          { featuretext: "OPTIONAL INSTANT PROCESSING. Payments made within seconds. This is the digital age - should your payments not reflect that?" },
+          { featureText: "EASY CUSTOMIZATION. Match your brand by customizing the logo, font, and colors." },
+          { featureText: "FEWER FIELDS. Get shoppers to the finish line faster. With fewer fields you significantly decrease the purchase time and reduce the possibility of user error." },
+          { featureText: "MOBILE READY. Provide the best checkout experience regardless of the device with checkout optimized for mobile." }
+        ]
+      },
+      {
+        linkText: "Bill Payments & Invoicing",
+        headerLabel: "Bill Payments & Invoicing",
+        headerText: "Collect, send, manage and maximize payments. Invoicing and bill payments for ambitious businesses.",
+        contentText: `
+          Canada's favourite way to pay has been fully automated by Paybilt, making it the perfect payment method for high-volume online businesses.
+          Add Paybilt's e-Transfer alongside your current payment options and gain access to more than 24 million active monthly e-Transfer® users.
+          Overnight. The proof is in the payments.
+            <ul>
+              <li>82% of Canadians would like to see businesses accept Interac e-Transfer® as a payment method.</li>
+              <li>68% of Canadians would rather pay with an Interac e-Transfer® than a credit card.</li>
+              <li>76% of Canadians would prefer to be refunded with an Interac e-Transfer®. The numbers don't lie. Canadians love it.</li>
+            </ul>`,
+        features: [
+          { featureText: "Skip excessive costs, complications, and risks." },
+          { featureText: "Increase your sales." },
+          { featureText: "Optimize operations." },
+          { featureText: "Access detailed reports on payments, settlements, refunds and more." },
+          { featureText: "Improve UX/UI." },
+          { featureText: "PCI compliant to ensure all sensitive data is stored and tokenized." },
+          { featureText: "Easy API integration and onboarding." },
+          { featureText: "Built for speed and scale." }
+        ]
+      },
+      {
+        linkText: "QR code scanning",
+        headerLabel: "QR code scanning",
+        headerText: "For both in-person and online.",
+        contentText: `
+          As payment technology evolves and customers seek new ways to pay, QR Codes are becoming more popular as an alternative solution
+          for Merchants to expand their customer's payment options and use their mobile devices to pay.
+
+          How it works?
+
+          Buyers - Just scan, pay, and go. Customers open your app, select 'Scan/Pay' and hold their phone up to the QR code. Then,
+          the customer enters the amount and taps 'Send'. Paying with our app means you don't have to handle cash or credit cards when
+          you pay in person. All you need to do is scan a seller's QR code when you pay. Sellers - Get set to sell with QR codes. If you're
+          about to start your own business, or already up and running, it's easy to accept touch-free payments in-person. Just download and
+          display your Paybilt QR code, so your customers can scan it with their app. No need for hardware, software, or a card reader.
+
+          Paybilt offers two QR Code based payment solutions to help both businesses and customers:
+
+          Paybilt Merchant-Presented QR Code (MPQR):
+          The merchant displays the QR Code in-store via a decal or through a terminal, and the Card
+          Members use their smartphone to scan the QR Code.
+
+          Paybilt Consumer-Presented QR Code (CPQR):
+          Card Members generate a QR Code from their mobile wallet or banking app and the Merchant scans the QR Code as a form of payment.`,
+        features: []
+      }
+    ]
+  },
+  {
+    label: "International",
+    items: [
+      {
+        linkText: "WeChat Pay",
+        headerLabel: "WeChat Pay",
+        headerText: "China's most popular social app,WeChat, has a payment side.",
+        contentText: `
+          WeChat connects millions of users to services and businesses in China (and around the world). WeChat Pay
+          has quickly become an important payment method for businesses wanting to reach the Chinese market, both at home and abroad.`,
+        features: [
+          { featureText: "Almost 1B active wallet users." },
+          { featureText: "WeChat Pay is responsible for 34% of total mobile data traffic in China" },
+          { featureText: "Cross-border settlements: payouts supported in multiple currencies" }
+        ]
+      },
+      {
+        linkText: "AliPay",
+        headerLabel: "AliPay",
+        headerText: "Paybilt makes it easy to capture sales from people visiting Canada from China.",
+        contentText: `
+          China's eCommerce sector is by far the largest on the planet. Seamlessly tap into this lucrative market, with travelers buying gifts
+          and sending care packages to loved ones attending school in Canada's major cities.
+
+          How it works?
+
+          Alipay enables users can pay for goods and services via a mobile app or a web browser using their Alipay app login credentials.`,
+        features: []
+      },
+      {
+        linkText: "Boleto",
+        headerLabel: "Boleto",
+        headerText: "The official Brazilian cash-based payment method.",
+        contentText: `
+          The only payment method available for the 17% of Brazilian shoppers who don't have a bank account.
+
+          How it works?
+
+          Customers complete transactions by receiving a voucher that states the amount to pay for services or goods. The Boleto can be
+          paid in a variety of different ways, including at authorized agencies or banks, ATMs, or online bank portals. Customers must
+          pay the Boleto before its expiration date and will receive payment confirmation after 1 business day. Following the payment
+          confirmation, disbursement funds will be available after 2 business days.
+          `,
+        features: [
+          { featureText: "Settlement within 3-4 days" },
+          { featureText: "Lower risk of fraud" },
+          { featureText: "No chargebacks" },
+          { featureText: "No credit limits" }
+        ]
+      },
+    ]
+  },
+  {
+    label: "Payments (IN)",
+    items: [
+      {
+        linkText: "Interac e-Transfer®",
+        headerLabel: "Interac e-Transfer®",
+        headerText: "The undisputed champion of lightning-fast disbursement.",
+        contentText: `
+          Canada's favourite way to send and receive money has been automated by Paybilt and is ready for high-volume online businesses.
+          Instantly gain access to a low-cost payment network boasting over 24 million active monthly e-Transfer® users.`,
+        features: [
+          { featureText: "All Canadian banks supported" },
+          { featureText: "Built-in redundancy" },
+          { featureText: "Extremely popular" },
+          { featureText: "Guaranteed funds" }
+        ]
+      },
+      {
+        linkText: "Interac® Online",
+        headerLabel: "Interac® Online",
+        headerText: "Tried and true instant payment method for select debit cards.",
+        contentText: `
+          Pay directly and instantly from a chequing account using an online banking username and password. Users log in to their online
+          banking through the secure Interac® payment hosted page, choose an account, and accept the payment. The low fees make this an attractive option for business.`,
+        features: [
+          { featureText: "Instant form of payment." },
+          { featureText: "Uses Interac's secure rails." },
+          { featureText: "Low fees." },
+          { featureText: "Trusted Interac® brand." }
+        ]
+      },
+      {
+        linkText: "EFT Online Banking Payment (Pay with Bank)",
+        headerLabel: "EFT Online Banking Payment (Pay with Bank)",
+        headerText: "",
+        contentText: `
+          Customers use their online banking username and password to pay instantly from their own bank account. Seamlessly facilitate EFT
+          payment processing by connecting to Canada's banking system. This instant, guaranteed payment method allows customers to make
+          account-to-account payments and withdrawals using their own money. Advanced fraud controls and indemnification make this one
+          of the quickest and safest ways to accept payment from your customers, for all types of businesses.`,
+        features: [
+          { featureText: "Guaranteed payment" },
+          { featureText: "No chargebacks" },
+          { featureText: "Full indemnified for some industries" },
+          { featureText: "Quick and easy to use" }
+        ]
+      },
+      {
+        linkText: "eCheque",
+        headerLabel: "eCheque",
+        headerText: "",
+        contentText: `
+          Essentially an “electronic cheque.” Customers make payments online using the information from the bottom of a cheque.
+          The original EFT or “electronic cheque” is still relevant in the 21st century and beyond. It uses account information collected
+          from the user through an online form and is submitted to the banking network electronically.`,
+        features: [
+          { featureText: "Supported by all banks" },
+          { featureText: "Cost-effective" },
+          { featureText: "Supports one-click checkout" },
+          { featureText: "One-time or recurring billing" }
+        ]
+      }
+    ]
+  },
+  {
+    label: "Payments (OUT)",
+    items: [
+
+    ]
+  }
+];

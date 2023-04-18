c8010937145aca4afbe5a51ea752447218ae9d86
Lester D. Bravo <dago@outlook.com>
2023-04-18 13:04:17 -0600

    Latest changes

--- assets/js/animation-scripts/index.js
+++ assets/js/animation-scripts/index.js
@@ -1,16 +1,39 @@
 gsap.registerPlugin(ScrollTrigger);
+
 gsap.to(".perfecting-image", {
   scrollTrigger: ".perfecting-image",
   x: 0,
   duration: 1
 });
+
 gsap.to(".text1-span", {
   scrollTrigger: ".text1-span",
   x: 0,
   duration: 1
 });
+
 gsap.fromTo(".particles", { x: -100 }, { x: 0, duration: 2 });
+gsap.to(".particles", {
+  x: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".particles",
+    scrub: 1,
+    start: "top center",
+    end: "bottom -30%"
+  }
+});
+
 gsap.fromTo(".girl", { x: 100 }, { x: 0, duration: 2.5 });
+gsap.to(".girl", {
+  x: -150,
+  scrollTrigger: {
+    trigger: ".girl",
+    scrub: 1,
+    start: "top center",
+    end: "bottom -30%"
+  }
+});
 
 gsap.to('.tennis-shoes-image', {
   x: 0,
@@ -44,6 +67,28 @@ gsap.to('#perks-text2', {
     trigger: "#perks-text2",
     scrub: 1,
     start: "top 60%",
+    end: "bottom -30%"
+  }
+});
+
+gsap.to('.amount-image', {
+  scale: 1,
+  ease: "expo",
+  scrollTrigger: {
+    trigger: ".amount-image",
+    scrub: 1,
+    start: "top 80%",
+    end: "bottom -30%"
+  }
+});
+
+gsap.to('.overnight', {
+  scale: 1,
+  ease: "expo",
+  scrollTrigger: {
+    trigger: ".overnight",
+    scrub: 1,
+    start: "top 80%",
     end: "bottom -30%"
   }
 });

b01857fe2c6e89fee7f503d9a17fd3637f9e2163
Lester D. Bravo <dago@outlook.com>
2023-04-25 20:08:37 -0600

    Latest changes

--- assets/js/swipper-settings.js
+++ assets/js/swipper-settings.js
@@ -1,33 +1,16 @@
-const ltMd = window.matchMedia('only screen and (max-width: 767px)').matches;
-if (!ltMd) {
-  const swiper = new Swiper('.products-swiper', {
-    speed: 3000,
-    autoplay: {
-      delay: 5,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true
-    },
-    loop: true,
-    slidesPerView: 5,
-    watchSlidesProgress: false,
-    spaceBetween: 0,
-    grabCursor: true
-  });
-} else {
-  const swiper = new Swiper('.products-swiper', {
-    speed: 3000,
-    autoplay: {
-      delay: 5,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true
-    },
-    loop: true,
-    slidesPerView: 1,
-    watchSlidesProgress: false,
-    spaceBetween: 0,
-    grabCursor: true
-  });
-}
+const swiper = new Swiper('.products-swiper', {
+  speed: 3000,
+  autoplay: {
+    delay: 5,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  },
+  loop: true,
+  slidesPerView: isSm ? 1 : isMd ? 2 : 5,
+  watchSlidesProgress: false,
+  spaceBetween: 0,
+  grabCursor: true
+});
 
 const perksSwiper = new Swiper('.perks-swiper', {
   loop: true,
